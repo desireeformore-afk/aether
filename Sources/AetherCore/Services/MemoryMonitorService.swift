7b2c13fee5d3eb242da74190621721616d8ab444
Hermes <hermes@aether>
2026-04-18 17:22:16 +0000

    fix: mark getMemoryUsage as nonisolated to allow mach_task_self_ access

--- Sources/AetherCore/Services/MemoryMonitorService.swift
+++ Sources/AetherCore/Services/MemoryMonitorService.swift
@@ -127,13 +127,13 @@ public final class MemoryMonitorService: ObservableObject {
         }
     }
 
-    private func getMemoryUsage() -> (used: UInt64, total: UInt64) {
+    private nonisolated func getMemoryUsage() -> (used: UInt64, total: UInt64) {
         var info = mach_task_basic_info()
         var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size) / 4
 
         let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
             $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                task_info(mach_task_self(), task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
+                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
             }
         }
 
