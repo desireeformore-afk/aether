d9e2c6eba48da353675c1c2d68ce7ada5c51bd51
Hermes <hermes@aether>
2026-04-18 10:48:54 +0000

    fix: add type property to Playlist model

--- Sources/AetherCore/Models/Playlist.swift
+++ Sources/AetherCore/Models/Playlist.swift
@@ -5,17 +5,20 @@ public struct Playlist: Identifiable, Sendable {
     public let id: UUID
     public var name: String
     public var url: URL
+    public var type: PlaylistType
     public var channels: [Channel]
 
     public init(
         id: UUID = UUID(),
         name: String,
         url: URL,
+        type: PlaylistType = .m3u,
         channels: [Channel] = []
     ) {
         self.id = id
         self.name = name
         self.url = url
+        self.type = type
         self.channels = channels
     }
 }
