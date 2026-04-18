287023a416e858631f963981768e277524e5f57d
Hermes <hermes@aether>
2026-04-18 17:20:00 +0000

    fix: mark mach_task_self_ as nonisolated(unsafe) for Swift 6 concurrency

--- Sources/AetherCore/Services/MemoryMonitorService.swift
+++ Sources/AetherCore/Services/MemoryMonitorService.swift
@@ -133,7 +133,8 @@ public final class MemoryMonitorService: ObservableObject {
 
         let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
             $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
+                nonisolated(unsafe) let taskSelf = mach_task_self_
+                return task_info(taskSelf, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
             }
         }
 
