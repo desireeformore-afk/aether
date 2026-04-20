f453b4ae82b742ba28a3976a82ecf2850f47e7ed
Hermes <hermes@aether>
2026-04-20 14:29:32 +0000

    fix: remove duplicate Color(hex:) in HomeView (ambiguous with AetherCore)

--- Sources/AetherApp/Views/HomeView.swift
+++ Sources/AetherApp/Views/HomeView.swift
@@ -452,15 +452,4 @@ struct WelcomeView: View {
     }
 }
 
-// MARK: - Color(hex:) helper
 
-private extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: .init(charactersIn: "#"))
-        let int = UInt64(hex, radix: 16) ?? 0
-        let r = Double((int >> 16) & 0xFF) / 255
-        let g = Double((int >> 8) & 0xFF) / 255
-        let b = Double(int & 0xFF) / 255
-        self.init(red: r, green: g, blue: b)
-    }
-}
